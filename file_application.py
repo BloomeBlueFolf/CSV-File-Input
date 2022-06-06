f04c10c2381aabfb74ca821dfba6304be2f42c7c
fege <f.ege@web.de>
2022-06-06 14:40:49 +0200

    implementing more featues

--- file_application.py
+++ file_application.py
@@ -1,13 +1,13 @@
 import pandas as pd
+import termcolor as tc
 
 
 def create_csv_file():
-    list_prenames = []
-    list_lastnames = []
-    list_courses = []
+    dictionary = {}
 
-    print("Welcome! \nHere you can create a csv file.\n\n-c for creating\n-s for saving\n-r "
-          "for reading\n-q for quitting\n")
+    description = "\n\n-a for adding data\n-c for creating\n-h for help\n-q for quitting" \
+                  "\n-r for reading\n-s for saving\n"
+    print("Welcome! \nHere you can create a csv file.{}".format(description))
 
     while True:
 
@@ -18,22 +18,53 @@ def create_csv_file():
             break
 
         if statement == "-c":
-            list_prenames, list_lastnames, list_courses = create_dataset(list_prenames, list_lastnames, list_courses)
+            dictionary = create_dataset(dictionary)
+            continue
+
+        if statement == "-d":
+            dictionary = delete_last_row(dictionary)
+            continue
+
+        if statement == "-a":
+            if not dictionary.keys():
+                print("\nThere is no table header created. Create a table header to add data!\n")
+                continue
+
+            else:
+                print("Data input started.\n")
+
+                next_row = True
+                while next_row:
+                    dictionary = add_data(dictionary)
+                    continue_data_input = input("\nIf you want to continue with adding data press \"y\".\n")
+                    if continue_data_input != "y":
+                        next_row = False
+                continue
+
+        if statement == "-h":
+            print("\n-a  You can add a row of data to an already created table.")
+            print("\n-c  Lets you create a table header.")
+            print("\n-h  Shows you a description to a command's function.")
+            print("\n-q  Eventually exits the application.")
+            print("\n-r  Displays you the content of an arbitrary csv file.")
+            print("\n-s  Saves your created table as a csv file.")
             continue
 
         if statement == "-s":
             while True:
                 file_name = input("Please name your file to save it.\n") + ".csv"
-                print("{}} - y?\n".format(file_name))
-                y_or_n = input()
-                if y_or_n == "y":
-                    dictionary = {"Prename": list_prenames, "Lastname": list_lastnames, "Course": list_courses}
+                print("{} - y?\n".format(file_name))
+                accept = input()
+                if accept == "y":
                     dataframe = pd.DataFrame(dictionary)
                     dataframe.to_csv(file_name, index=False)
+                    print("File created.")
                     break
 
                 else:
                     continue
+
+            continue
 
         if statement == "-r":
             while True:
@@ -47,46 +78,32 @@ def create_csv_file():
                     break
 
                 except FileNotFoundError:
-                    print("There's not such a file. Try again!\n")
+                    print(tc.colored("There's not such a file. Try again!\n"))
                     continue
 
         else:
-            print("This was no valid statement.\n\n-c for creating\n-s for saving\n-q for quitting\n")
+            print(tc.colored("\nThis was no valid statement.{}".format(description)))
             continue
 
 
-def create_dataset(list_prenames, list_lastnames, list_courses):
-    prename, lastname, course = add_row()
-
-    list_prenames.append(prename)
-    list_lastnames.append(lastname)
-    list_courses.append(course)
-
-    return list_prenames, list_lastnames, list_courses
-
-
-def add_row():
-    prename = input("Prename: ")
-    lastname = input("Lastname: ")
-    course = input("Course: ")
-    return prename, lastname, course
-
-
-def create_file():
+def create_dataset(dictionary):
     columns = input("How many columns shall your file have?  ")
-    dictionary = {}
 
-    print("\nCreated a file with %s columns.\n" % columns)
-    print("\nPlease name the columns.\n")
-    for i in range(int(columns)):
-        name = input("\nName of column {}:  ".format(i + 1))
-        dictionary[name] = []
+    try:
 
-    return dictionary
+        for i in range(int(columns)):
+            name = input("\nName of column {}:  ".format(i + 1))
+            dictionary[name] = []
+        print("\nCreated a file with %s columns.\n" % columns)
+        print("\nPlease name the columns.\n")
+
+        return dictionary
+
+    except ValueError:
+        print(tc.colored("\n Please enter a valid number!\n", "red"))
 
 
 def add_data(dictionary):
-    print("Data input started.\n")
 
     for table_element in dictionary.keys():
         data_input = input("\n{}:  ".format(table_element))
@@ -95,15 +112,16 @@ def add_data(dictionary):
     return dictionary
 
 
-def test():
-    print(add_data(create_file()))
+def delete_last_row(dictionary):
+
+    for table_element in dictionary.keys():
+        dictionary[table_element].remove(-1)
+
+    return dictionary
 
 
 if __name__ == '__main__':
-    #create_csv_file()
-    #create_file()
-    test()
-
+    create_csv_file()
 
     # deleting a row
     # json
